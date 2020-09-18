40d374079bdd55a454d71d900fe629b03671d5db
Xavier Ritch <xritch89@hotmail.com>
2020-09-18 12:15:21 -0400

    post working

--- src/app.js
+++ src/app.js
@@ -1,29 +1,27 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const winston = require('winston')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const winston = require("winston");
+const { v4: uuid } = require('uuid');
+const app = express();
 
-const app = express()
-
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 const logger = winston.createLogger({
-  level: 'info',
+  level: "info",
   format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'info.log' })
-  ]
+  transports: [new winston.transports.File({ filename: "info.log" })],
 });
 
-if (NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
+if (NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
 }
 
 // const bookmarks = [{
@@ -34,44 +32,64 @@ if (NODE_ENV !== 'production') {
 //   id = "",
 // }];
 
-const list = [];
+const bookmarks = [];
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' })
+    return res.status(401).json({ error: "Unauthorized request" });
   }
   // move to the next middleware
-  next()
-})
-
-app.get('/bookmark', (req, res) => {
-  res
-    .json('hello');
+  next();
 });
 
-app.get('/list', (req, res) => {
-  res
-    .json(lists);
+app.get("/bookmarks", (req, res) => {
+  res.json(bookmarks);
+});
+
+app.post("/bookmarks", express.json(), (req, res) => {
+  const { title, desc, rating, url } = req.body;
+  const id = uuid();
+  const newBookmark = {
+    id,
+    title,
+    desc,
+    rating,
+    url,
+  };
+  if (!title) {
+    return res.status(400).send("Please enter a title");
+  }
+  if (!desc) {
+    return res.status(400).send("Please enter a description");
+  }
+  if (!rating || rating > 5 || rating < 1) {
+    return res.status(400).send("Please enter a valid rating from 1 to 5");
+  }
+  if (!url || !url.includes("http")) {
+    return res.status(400).send("Please enter a valid url");
+  }
+
+  bookmarks.push(newBookmark);
+  res.json(bookmarks);
 });
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
   } else {
-    console.error(error)
-    response = { message: error.message, error }
+    console.error(error);
+    response = { message: error.message, error };
   }
-  res.status(500).json(response)
-})
-  
+  res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;

7d10be4938b534d86a805538ba66b875c5f99af1
Xavier Ritch <xritch89@hotmail.com>
2020-09-21 09:07:26 -0400

    added missing import

--- src/bookmark/bookmark-router.js
+++ src/bookmark/bookmark-router.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const { v4: uuid } = require("uuid")
-const { bookmarks } = require('../store')
+const express = require("express");
+const { v4: uuid } = require("uuid");
+const { bookmarks } = require("../store");
+const logger = require("../logger");
 
-const bookmarkRouter = express.Router()
-const bodyParser = express.json()
+const bookmarkRouter = express.Router();
+const bodyParser = express.json();
 
 bookmarkRouter
-  .route('/')
-  .get((req,res) => {
+  .route("/")
+  .get((req, res) => {
     res.json(bookmarks);
   })
   .post(bodyParser, (req, res) => {
-    const { title, desc, rating, url } = req.body;
+    const { title, description, rating, url } = req.body;
     const id = uuid();
     const newBookmark = {
       id,
       title,
-      desc,
+      description,
       rating,
       url,
     };
     if (!title) {
       return res.status(400).send("Please enter a title");
     }
-    if (!desc) {
+    if (!description) {
       return res.status(400).send("Please enter a description");
     }
     if (!rating || rating > 5 || rating < 1) {
@@ -32,38 +33,38 @@ bookmarkRouter
     if (!url || !url.includes("http")) {
       return res.status(400).send("Please enter a valid url");
     }
-  
+
     bookmarks.push(newBookmark);
     res.json(bookmarks);
   });
-  
+
 bookmarkRouter
-  .route('/:id')
+  .route("/:id")
   .get((req, res) => {
     const { id } = req.params;
     const bookmark = bookmarks.find((c) => c.id == id);
-  
+
     // make sure we found a card
     if (!bookmark) {
       logger.error(`bookmark with id ${id} not found.`);
       return res.status(404).send("Bookmark Not Found");
     }
-  
+
     res.json(bookmark);
   })
   .delete((req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  const index = bookmarks.findIndex((u) => u.id === id);
+    const index = bookmarks.findIndex((u) => u.id === id);
 
-  // make sure we actually find a user with that id
-  if (index === -1) {
-    return res.status(404).send("bookmark not found");
-  }
+    // make sure we actually find a user with that id
+    if (index === -1) {
+      return res.status(404).send("bookmark not found");
+    }
 
-  bookmarks.splice(index, 1);
+    bookmarks.splice(index, 1);
 
-  res.send("bookmark deleted");
-});
+    res.send("bookmark deleted");
+  });
 
-module.exports = bookmarkRouter
+module.exports = bookmarkRouter;
